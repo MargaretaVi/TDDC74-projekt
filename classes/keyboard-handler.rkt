1a3588bf1c60ea46347c0b50774543f42b7db5bc
Margareta <margareta.vi@gmail.com>
2017-04-27 15:48:37 +0200

    add comments to the functions

--- classes/keyboard-handler.rkt
+++ classes/keyboard-handler.rkt
@@ -1,30 +1,36 @@
 #lang racket
 (provide keyboard-handler%)
 
+
 (define keyboard-handler%
   (class object%
-
+    ;;Members of class
     (init-field
      
      [*list-of-keys-pressed* (make-hash)])
 
-
+    ;; Functions that returns the class members.
     (define/public (get-list-of-keys-pressed)
       (hash-keys *list-of-keys-pressed*))
 
+    ;; ------------------------------------
+
+    ;;Adds a key to the hash-table *list-of-key-pressed*.
     (define/public (add-key key)
       (if (hash-has-key? *list-of-keys-pressed* (string key))
           #f
           (begin
             (hash-set! *list-of-keys-pressed* (string key) key)
             #t)))
-
+    
+    ;;Removes a key from the hash-table *list-of-key-pressed*.
     (define/public (delete-key key)
       (if (hash-has-key? *list-of-keys-pressed*  (string key))
           (hash-remove! *list-of-keys-pressed* (string key))
           #f))
 
+    ;;Checks if key exist in hash-table
     (define/public (key-pressed? key)
-      (hash-has-key? *list-of-keys-pressed* key))
+      (hash-has-key? *list-of-keys-pressed* (string key)))
     
     (super-new)))
