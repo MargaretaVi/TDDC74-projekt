244b4a5c6bdb1ef238142cab8de2f15d00db6ec1
Margareta <margareta.vi@gmail.com>
2017-05-03 13:35:49 +0200

    add changes to the variable names

--- classes/keyboard-handler.rkt
+++ classes/keyboard-handler.rkt
@@ -7,30 +7,30 @@
     ;;Members of class
     (init-field
      
-     [*list-of-keys-pressed* (make-hash)])
+     [_list-of-keys-pressed (make-hash)])
 
     ;; Functions that returns the class members.
     (define/public (get-list-of-keys-pressed)
-      (hash-keys *list-of-keys-pressed*))
+      (hash-keys _list-of-keys-pressed))
 
     ;; ------------------------------------
 
     ;;Adds a key to the hash-table *list-of-key-pressed*.
     (define/public (add-key key)
-      (if (hash-has-key? *list-of-keys-pressed* (string key))
+      (if (hash-has-key? _list-of-keys-pressed (string key))
           #f
           (begin
-            (hash-set! *list-of-keys-pressed* (string key) key)
+            (hash-set! _list-of-keys-pressed (string key) key)
             #t)))
     
     ;;Removes a key from the hash-table *list-of-key-pressed*.
     (define/public (delete-key key)
-      (if (hash-has-key? *list-of-keys-pressed*  (string key))
-          (hash-remove! *list-of-keys-pressed* (string key))
+      (if (hash-has-key? _list-of-keys-pressed  (string key))
+          (hash-remove! _list-of-keys-pressed (string key))
           #f))
 
     ;;Checks if key exist in hash-table
     (define/public (key-pressed? key)
-      (hash-has-key? *list-of-keys-pressed* (string key)))
+      (hash-has-key? _list-of-keys-pressed (string key)))
     
     (super-new)))
