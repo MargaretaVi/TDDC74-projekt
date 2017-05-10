90d3c96345dc6c7708e58be4af3d65566b82c0cd
erili714 <erili714@student.liu.se>
2017-05-10 15:12:39 +0200

    Some fix on keyboard

--- classes/TEstkey.rkt
+++ classes/TEstkey.rkt
@@ -1,46 +1,45 @@
 #lang racket/gui
+(require "characters.rkt")
+(require "player.rkt")
 
+(define *character* (new character% [_height 1] [_width 1])) 
 (define a-window (new frame%
-                          [label "Prutt"]
-                          [width 800]
-                          [height 600]))
+                          [label "Canvas"]
+                          [width 500]
+                          [height 500]))
 (send a-window show #t)
-
-(define xpos 0)
-(define ypos 0)
 
 (define (drawing-proc canvas dc)
   (let ((our-picture (make-object bitmap% "AA.png")))
-    (send dc translate xpos ypos)
-    (send dc draw-bitmap our-picture 100 100)
-    (send dc translate (- xpos) (- ypos))))
+    (send dc translate (send *character* get-x-pos) (send *character* get-y-pos))
+    (send dc draw-bitmap our-picture 100 100)))
 
 (define a-canvas%
   (class canvas%
     (init-field keyboard-handler)
 
-    (define/override (on-char key-event)
-      (keyboard-handler key-event))
+    (define/override (on-char key-fnc)
+      (keyboard-handler key-fnc))
     (super-new)))
 
-(define (key-proc key-event)
+(define (key-fnc key-event)
   (let
       ((key-tag (send key-event get-key-code)))
   (cond
     ((eq? key-tag #\space)
-     (set! shootybooty (shooty true)))
+     (send *character* fire))
     ((eq? key-tag #\a)
-     (set! xpos (- xpos 15)))
+     (send *character* move-x -1))
     ((eq? key-tag #\d)
-     (set! xpos (+ xpos 15)))
+     (send *character* move-x 1))
     ((eq? key-tag #\w)
-     (set! ypos (- ypos 15)))
+     (send *character* move-y -1))
     ((eq? key-tag #\s)
-     (set! ypos (+ ypos 15)))))
+     (send *character* move-y 1))))
   (send a-window refresh))
 
 (define our-canvas (new a-canvas%
                          [parent a-window]
                          [paint-callback drawing-proc]
-                         [keyboard-handler key-proc]))
-(send our-canvas focus)
+                         [keyboard-handler key-fnc]))
+;(send our-canvas focus)
